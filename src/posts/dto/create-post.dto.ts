402beb29e44184a1d014bede53a1bed165e9b071
Artem Abdullin <artem.abdullin@anoda.mobi>
2022-06-04 23:08:23 +0300

    Added Swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,22 +1,28 @@
 import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   link: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   guid: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   creator: string;
